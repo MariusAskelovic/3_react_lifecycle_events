6d3ab48b996920f2e49ce965c13a2c827b806ea4
MariusAskelovic <72157256+MariusAskelovic@users.noreply.github.com>
2023-08-08 09:54:20 +0300

    useState click

--- src/components/counter/Counter.jsx
+++ src/components/counter/Counter.jsx
@@ -1,13 +1,23 @@
 import Grid from "../UI/Grid";
+import { useState } from 'react';
 
 function Counter() {
+    console.log('<Counter generated />');
+    const [countNum, setCountNum] = useState(0);
+    function goUp() {
+        setCountNum(countNum + 1)
+    }
+    function goDown() {
+        setCountNum(countNum - 1)
+    }
+
     return (
         <div className="counter">
             <h2>Label</h2>
-            <p className="counterValue">0</p>
+            <p className="counterValue">{countNum}</p>
             <Grid cols='2'>
-                <button>UP</button>
-                <button>DOWN</button>
+                <button onClick={goUp}>UP</button>
+                <button onClick={goDown}>DOWN</button>
             </Grid>
         </div>
     )
