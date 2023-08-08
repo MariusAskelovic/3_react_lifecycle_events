0933534ffcdffbdae59dc0f722f4dacbe9d4f1cc
MariusAskelovic <72157256+MariusAskelovic@users.noreply.github.com>
2023-08-08 10:34:09 +0300

    CounterList

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import './styles/index.css'
 import './styles/App.css'
 import CardList from './components/CardList';
 import Counter from './components/counter/Counter';
+import CounterList from './components/counter/CounterList';
 
 function App() {
   return <div className="app">
     <h1>Hello Vite</h1>
     {false && <CardList />}
     <Counter />
+    <CounterList />
   </div>
 }
 
